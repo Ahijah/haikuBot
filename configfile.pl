7b672acd6b9e79ebf9d03fc9be54813f099e76e1
Xafloc <xafloc@rego.ai>
2020-05-04 13:32:49 -0400

    Separation of configuration, along with swap out to ? for trigger

--- configfile.pl
+++ configfile.pl
@@ -7,6 +7,7 @@
 	dbpass => '',
 	botOwner => 'Xafloc',
 	URL => 'https://apps.rego.ai/HaikuBot',
+	# APIURL => 'https://api.datamuse.com/words?md=s&max=1&sp=',
 	# Be sure to escape special characters
 	trigger => '?haiku', 
 	triggerStats => '?haikustats',
@@ -14,6 +15,8 @@
 	triggerHelp => '?haikuhelp',
 	triggerList => '?haikulist',
 	triggerTop => '?haikutop',
+	trigger5 => '?haiku5',
+	trigger7 => '?haiku7',
 
 	##############################################
 	# Profanity Settings
@@ -39,10 +42,10 @@
 		"?haiku - (Generates a random Haiku)\n" .
 		"?haikustats - (Displays HaikuBot statistics)\n" .
 		"?haikuvote <Haiku ID> - (Upvotes Haiku matching ID - Max one vote per user/haiku)\n" .
-		"?newhaiku5 <5 syllable haiku line> - (Creates 5 syllable haiku line)\n" .
-		"?newhaiku5 <5 syllable haiku line>|<1 or 3> - (Creates 5 syllable haiku line w/ forced 1 or 3 position)\n" .
-		"  E.g. !newhaiku5 this must be the end|3\n" .
-		"?newhaiku7 <7 syllable haiku line> - (Creates a new 7 syllable haiku line)\n" .
+		"?haiku5 <5 syllable haiku line> - (Creates 5 syllable haiku line)\n" .
+		"?haiku7 <5 syllable haiku line>|<1 or 3> - (Creates 5 syllable haiku line w/ forced 1 or 3 position)\n" .
+		"  E.g. !haiku5 this must be the end|3\n" .
+		"?haiku7 <7 syllable haiku line> - (Creates a new 7 syllable haiku line)\n" .
 		"?haikutop - (PMs the top 5 voted randomly generated haiku)\n" .
 		" --Web List of Generated Haiku: https://apps.rego.ai/HaikuBot",
 }
